0dffdd10478dc0ef7a784cf15532b054ad0a61e1
daiViis <137108163+daiViis@users.noreply.github.com>
2023-08-28 13:12:45 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -17,33 +17,33 @@
 
 <h1>Záznam csv.csv</h1>
 
-</body>
+        <?php 
+
+            //zahrnuji funkce a vlastnosti třídy File
+
+            include ("Classes/File.php");
+
+            //načítám soubor csv.csv přes konstruktor třídy File a udávám kolik je na jednom řádku informací
+
+            $csv = new File("Files/csv.csv", 4);
+
+            //čtu soubor
+
+            echo $csv->readFile();
+
+            //výsledek čtení převádím do srozumitelné tabulky
+
+            echo $csv->printFile();
+
+            echo "<h1>CSV soubor do databáze</h1>";
+
+            //výsledek čtení také převádím do databáze
+
+            $csv->fileToDatabase($conn);
+
+        ?>
+
+        <p>David Cit</p>
+    
+    </body>
 </html>
-
-<?php 
-
-    //zahrnuji funkce a vlastnosti třídy File
-
-    include ("Classes/File.php");
-
-    //načítám soubor csv.csv přes konstruktor třídy File a udávám kolik je na jednom řádku informací
-
-    $csv = new File("Files/csv.csv", 4);
-
-    //čtu soubor
-
-    echo $csv->readFile();
-
-    //výsledek čtení převádím do srozumitelné tabulky
-
-    echo $csv->printFile();
-
-    echo "<h1>CSV soubor do databáze</h1>";
-
-    //výsledek čtení také převádím do databáze
-
-    $csv->fileToDatabase($conn);
-
-?>
-
-<p>David Cit</p>
